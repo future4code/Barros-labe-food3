da974848a04dcc5fb0737cd67abfb554b9b39f55
Ricardo Barros de Morais <ricardo_teleco@hotmail.com>
2022-09-21 16:51:50 -0300

    Criando o Projeto Labefood

---
+++ login-victor/fourfood/src/routes/Router.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import FeedPage from '../pages/feedPage/FeedPage'
+import LoginPage from '../pages/loginPage/LoginPage'
+import SignupPage from '../pages/signupPage/SignupPage'
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<LoginPage/>}/>
+            <Route path='/signup' element={<SignupPage/>}/>
+            <Route path='/feed' element={<FeedPage/>}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
