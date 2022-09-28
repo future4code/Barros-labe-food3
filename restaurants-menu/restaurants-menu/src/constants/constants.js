1be0c63b9bc4fd2bc060aa1207ff2a59d7de068a
Victor Lopes <vitoluiz1234@hotmail.com>
2022-09-28 13:23:57 -0300

    Delete restaurants-menu/restaurants-menu directory

--- restaurants-menu/restaurants-menu/src/constants/constants.js
+++
@@ -1,8 +0,0 @@
-
-export const BASE_URL = "https://us-central1-missao-newton.cloudfunctions.net/fourFoodA/restaurants"
-
- export const  Autorizacao = {
-    headers: {
-      auth: "fourFoodA"
-    }
-  };
